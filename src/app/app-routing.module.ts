d3306fed7f9af5cb88bddeba452a08b844460bce
Gaurav_Git <sonawanegs95@gmail.com>
2024-08-03 15:43:24 +0530

    commit update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { WorkComponent } from './work/work.component';
 
 const routes: Routes = [
+
   {path:'Dash',component:DashboardComponent},
   {path:'Port',component:PortfolioComponent},
-  
+  {path:'Work',component:WorkComponent},
+  {path:'',pathMatch:"full",component:DashboardComponent}
 ];
 
 @NgModule({
